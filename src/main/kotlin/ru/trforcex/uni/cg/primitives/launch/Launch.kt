6b4d07490a701dfa243312eea978d741c50109c2
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-09-10 17:18:44 +0300

    style: remove extra whitespace in Launch.kt

--- src/main/kotlin/ru/trforcex/uni/cg/primitives/launch/Launch.kt
+++ src/main/kotlin/ru/trforcex/uni/cg/primitives/launch/Launch.kt
@@ -4,7 +4,7 @@ import ru.trforcex.uni.cg.primitives.app.DrawingApplication
 import javax.swing.SwingUtilities
 
 fun main() {
-    SwingUtilities.invokeLater {  // Run all Swing stuff on the AWT thread.
+    SwingUtilities.invokeLater { // Run all Swing stuff on the AWT thread.
         DrawingApplication().isVisible = true
     }
 }
