8dacad13273db1de8cd32bad0220cea09b5dd6ab
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-09-10 04:23:38 +0300

    refactor: add constants for window size

--- src/main/kotlin/ru/trforcex/uni/cg/primitives/app/DrawingApplication.kt
+++ src/main/kotlin/ru/trforcex/uni/cg/primitives/app/DrawingApplication.kt
@@ -7,8 +7,20 @@ import javax.swing.JFrame
  * The main class of the drawing application.
  */
 class DrawingApplication : JFrame("My Drawing Application") {
+    companion object {
+        /**
+         * The width of the window in pixels.
+         */
+        const val WINDOW_WIDTH = 640
+
+        /**
+         * The height of the window in pixels.
+         */
+        const val WINDOW_HEIGHT = 360
+    }
+
     init {
-        setSize(640, 360)
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT)
         defaultCloseOperation = EXIT_ON_CLOSE
         isResizable = false
         add(DrawingPanel(), BorderLayout.CENTER)
