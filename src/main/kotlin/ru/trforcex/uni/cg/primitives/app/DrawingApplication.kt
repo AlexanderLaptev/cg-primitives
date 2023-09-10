2d688c5f158e3fdda8c4e66e819127073e9c3daf
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-09-10 04:20:55 +0300

    style: remove unused imports

--- src/main/kotlin/ru/trforcex/uni/cg/primitives/app/DrawingApplication.kt
+++ src/main/kotlin/ru/trforcex/uni/cg/primitives/app/DrawingApplication.kt
@@ -1,9 +1,6 @@
 package ru.trforcex.uni.cg.primitives.app
 
 import java.awt.BorderLayout
-import java.awt.Color
-import java.awt.Graphics
-import java.awt.Graphics2D
 import javax.swing.JFrame
 
 /**
