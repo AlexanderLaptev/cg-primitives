85fbae116ef45425c2b3cf64d2de8e1a0be5ba96
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-09-12 19:07:46 +0300

    feat: draw the keyboard cord

--- src/main/kotlin/ru/trforcex/uni/cg/primitives/app/DrawingPanel.kt
+++ src/main/kotlin/ru/trforcex/uni/cg/primitives/app/DrawingPanel.kt
@@ -4,10 +4,12 @@ import ru.trforcex.uni.cg.primitives.graphics.Monitor
 import ru.trforcex.uni.cg.primitives.util.clearComponentArea
 import ru.trforcex.uni.cg.primitives.util.fillPolygonOffset
 import ru.trforcex.uni.cg.primitives.util.setAntialiasingEnabled
+import java.awt.BasicStroke
 import java.awt.Color
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.Polygon
+import java.awt.geom.Path2D
 import javax.swing.JPanel
 
 /**
@@ -20,6 +22,7 @@ class DrawingPanel : JPanel() {
     companion object {
         private val BACKGROUND_COLOR: Color = Color.decode("#dbc7a2")
         private val KEYBOARD_COLOR = Color.decode("#333333")
+        private val KEYBOARD_CORD_COLOR = Color.decode("#2c2c2c")
         private val TABLE_COLOR: Color = Color.decode("#a38967")
 
         private const val TABLE_HEIGHT = 140
@@ -29,6 +32,15 @@ class DrawingPanel : JPanel() {
         private const val KEYBOARD_WIDTH = 360
         private const val KEYBOARD_OFFSET = 40
         private const val KEYBOARD_Y = 300
+
+        private const val KEYBOARD_CORD_WIDTH = 18
+        private const val KEYBOARD_CORD_THICKNESS = 5
+        private const val KEYBOARD_CORD_EDGE_OFFSET = 22
+        private val KEYBOARD_CORD_STROKE = BasicStroke(
+            KEYBOARD_CORD_THICKNESS.toFloat(),
+            BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_MITER
+        )
     }
 
     private var centerX = 0
@@ -40,6 +52,7 @@ class DrawingPanel : JPanel() {
         intArrayOf(0, -KEYBOARD_HEIGHT, -KEYBOARD_HEIGHT, 0),
         4
     )
+    private lateinit var keyboardCordCurve: Path2D.Float
 
     override fun paintComponent(g: Graphics?) {
         g as Graphics2D
@@ -53,10 +66,10 @@ class DrawingPanel : JPanel() {
 
     override fun doLayout() {
         super.doLayout()
-        // This method is early enough for us to update all polygons and paths before any rendering occurs.
-        updatePolygonsAndPaths()
         centerX = width / 2
         centerY = height / 2
+        // This method is early enough for us to update all polygons and paths before any rendering occurs.
+        updatePolygonsAndPaths()
     }
 
     private fun drawObjects(g: Graphics2D) {
@@ -79,6 +92,20 @@ class DrawingPanel : JPanel() {
             intArrayOf(height, height - TABLE_HEIGHT, height - TABLE_HEIGHT, height),
             4
         )
+        keyboardCordCurve = Path2D.Float().apply {
+            val cordTop: Float = (height - TABLE_HEIGHT).toFloat()
+            val cordBottom: Float = (centerY + KEYBOARD_Y - KEYBOARD_HEIGHT / 2).toFloat()
+
+            val cordLeft: Float = (centerX + KEYBOARD_WIDTH / 2 - KEYBOARD_OFFSET - KEYBOARD_CORD_EDGE_OFFSET).toFloat()
+            val cordRight: Float = cordLeft + KEYBOARD_CORD_WIDTH
+
+            moveTo(cordLeft, cordBottom)
+            curveTo(
+                cordLeft, cordTop,
+                cordRight, cordBottom,
+                cordRight, cordTop
+            )
+        }
         repaint()
     }
 
@@ -90,5 +117,12 @@ class DrawingPanel : JPanel() {
     private fun drawKeyboard(g: Graphics2D, x: Int, y: Int) {
         g.color = KEYBOARD_COLOR
         g.fillPolygonOffset(keyboardPolygon, x, y)
+        drawKeyboardCord(g)
+    }
+
+    private fun drawKeyboardCord(g: Graphics2D) {
+        g.color = KEYBOARD_CORD_COLOR
+        g.stroke = KEYBOARD_CORD_STROKE
+        g.draw(keyboardCordCurve)
     }
 }
